d10f52341e02dba63fa4435d8eeaae90c1affdbc
c1465333 <dimadc@cardiff.ac.uk>
2017-11-30 17:43:14 +0000

    Modify the way source and sensor info is used

--- Prepare/get_sensor_info.m
+++ Prepare/get_sensor_info.m
@@ -1,19 +1,12 @@
-function [] = get_sensor_info( dataset, output_file, varargin )
+function [neighbours] = get_sensor_info( dataset, varargin )
 % Inputs: dataset, output file path. Optional: 
 %  decoding_window: decoding window or window of
-%   interest (to save time axis for use in plotting etc.). If empty, whole
-%   axis saved.
-%  sample_rate: sampling rate (consider whether you'll be resampling in your analysis;
-%   this should be the final sample rate; default 1200).
-% Output: sensor neighbour structure + channel ids + time axis, saved to
-% .mat file that will be used in further decoding scripts.
-% Uses fieldtrip toolbox (Oostenveld et al., 2011).
-% DCD 2017
-
-p = inputParser;
-addParameter(p, 'decoding_window', []);
-addParameter(p, 'sample_rate', 1200);
-parse(p, varargin{:});
+%         interest (to save time axis for use in plotting etc.). If empty, whole
+%         axis saved.
+%  sample_rate: sampling rate - If you are going to resample the data, this
+%         need to be provided, otherwise you'll get a time axis based on the dataset sampling rate.
+% Output: sensor neighbour structure (includes labels, neighbours, and time).
+% Uses Fieldtrip toolbox (Oostenveld et al., 2011).
 
 hdr = ft_read_header(dataset);
 
@@ -22,23 +15,19 @@ cfg = [];
 cfg.method = 'template';
 cfg.template = [hdr.grad.type '_neighb.mat'];
 cfg.layout = [hdr.grad.type '.lay'];
-neighbours = ft_prepare_neighbours(cfg, hdr.grad); %#ok<NASGU> %neighblabel contains the neighbours of every single channel
+neighbours = ft_prepare_neighbours(cfg, hdr.grad);%neighblabel contains the neighbours of every single channel
 
-%select MEG channels
-for i = 1:length(hdr.label)
-    idx = cellfun('isempty',strfind(hdr.label, 'M'));
-end;
-chan_labels = hdr.label(~idx);
+p = inputParser;
+addParameter(p, 'decoding_window', []);
+addParameter(p, 'sample_rate', hdr.Fs);
+parse(p, varargin{:});
 
 if isempty(p.Results.decoding_window)
-    time = -(hdr.nSamplesPre/p.Results.sample_rate):(1/p.Results.sample_rate):((hdr.nSamples-hdr.nSamplesPre)/p.Results.sample_rate);
+    [neighbours.time] = deal(-(hdr.nSamplesPre/hdr.Fs):(1/p.Results.sample_rate):((hdr.nSamples-hdr.nSamplesPre)/hdr.Fs));
 else
-    time = p.Results.decoding_window:(1/p.Results.sample_rate):p.Results.decoding_window(2);
+    [neighbours.time] = deal(p.Results.decoding_window:(1/p.Results.sample_rate):p.Results.decoding_window(2));
 end;
 
-fprintf('\nSaving neighbourhood structure and labels for %d channels...', length(chan_labels));
-fprintf('\nSaving time axis from %f to %f...\n', time(1), time(end));
-save(output_file, 'neighbours', 'chan_labels', 'time'); %all useful things
 
 end
 
---
+++ Prepare/source_to_aal.m
@@ -0,0 +1,31 @@
+function [roi_idx] = source_to_aal(sourcemodel)
+%Outputs indices of inside sources in fieldtrip beamformer grid that fall within each of 90 AAL ROIs.
+%Inputs: - sourcemodelfile is filename of grid used in FT beamforming, i.e. standard_sourcemodel10mm.
+%        - savefile is filename to store cell array containing indices (roi_idx).
+
+atlas = ft_read_atlas('/cubric/software/MEG/fieldtrip-20161011/template/atlas/aal/ROI_MNI_V4.nii'); %load AAL atlas
+
+if ischar(sourcemodel)
+    load(['/cubric/software/MEG/fieldtrip-20161011/template/sourcemodel/' sourcemodel], 'sourcemodel'); %load sourcemodel
+end;
+sourcemodel = ft_convert_units(sourcemodel,'mm');
+
+cfg = [];
+cfg.interpmethod = 'nearest';
+cfg.parameter = 'tissue';
+sourcemodel2 = ft_sourceinterpolate(cfg, atlas, sourcemodel); %interpolate grid to atlas
+a = 1:length(sourcemodel.inside);
+inside = a(sourcemodel.inside);        %we want indices for the inside sources only (3,294 for a 10 mm grid)
+
+roi_idx = cell(1,90);
+
+for i = 1:90                                %loop through ROIs
+    indx = find(sourcemodel2.tissue==i);
+    roi_idx{i} = zeros(1,length(indx));
+    
+    for j = 1:length(indx)
+        roi_idx{i}(j) = find(inside==indx(j)); %store indices
+    end;
+end;
+
+end
