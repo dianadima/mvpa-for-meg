1dada36316b8f041471e67695ca240f52034c915
c1465333 <dimadc@cardiff.ac.uk>
2017-12-13 16:23:04 +0000

    Fix pseudotrial function

--- Prepare/create_pseudotrials.m
+++ Prepare/create_pseudotrials.m
@@ -1,56 +1,67 @@
-function [ newdata ] = create_pseudotrials( data, n_trials, n_perm, varargin )
+function [ newdata, newlabels ] = create_pseudotrials( data, labels, n_trials, n_perm)
 %Creates averages of trials to increase SNR for multivariate analysis.
 % Make sure that last dimension of data is trial dimension.
 % Optional parameter: cell array containing indices of trials
 % belonging to each condition. Example {1:40, 41:80}.
 
-if ~isempty(varargin)
-    conditions = varargin{1};
-    n_cond = length(varargin{1});    
-else
+trldim = ndims(data); %trials is last dimension, usually convenient for fieldtrip
+sz = [trldim 1:trldim-1]; data = permute(data, sz); %move trials to first dimension
+sz = size(data); %store new size
+
+if isempty(labels)
     n_cond = 1;
+    idx = repmat({':'}, 1, ndims(data));
+    idx{1} = 1:size(data, trldim);
+else
+    n_cond = length(unique(labels));
+    classes = unique(labels);
+    idx = repmat({':'}, n_cond, ndims(data));
+    for i = 1:n_cond
+        idx{i,1} = find(labels==classes(i));
+    end;
 end;
 
-trldim = ndims(data); %trials is last dimension
-newsz = [n_cond n_perm floor(size(data,trldim)/n_trials) size(data)];
+newsz = [n_perm floor(size(data,1)/n_trials) sz(2:end)];
 newdata = zeros(newsz);
-idx = repmat({':'},1,ndims(data)); %how we index data to be averaged
-
-for c = 1:n_cond
+newlabels = [];
     
-    for p = 1:n_perm
-        
-        if n_cond>1
-            
-            idx{trldim} = conditions{c};
-            tmpdata = data(idx{:});
-            tmpdata = shuffle(tmpdata, trldim);
-            tp = 1:n_trials:length(conditions{c})-n_trials+1;
-            
-        else
-            tmpdata = shuffle(data,trldim);
-            tp = 1:n_trials:size(data,trldim)-n_trials+1;
-            
-        end;
+for p = 1:n_perm
+    
+    condata = [];
+    
+    for c = 1:n_cond
+    
+        tmpdata = data(idx{c,:});
+        tmpdata = shuffle(tmpdata, 1);
+        tp = 1:n_trials:size(tmpdata, 1)-n_trials+1;
+        tmpsz = [length(tp) sz(2:end)]; 
+        avedata = zeros(tmpsz);
+        tmpidx = idx;
         
         for t = 1:length(tp)
             
-            idx{trldim} = tp(t):tp(t)+n_trials-1;
-            pseudo = tmpdata(idx{:});
-            pseudo = squeeze(nanmean(pseudo,trldim));
-            newdata(c,p,t,:) = pseudo(:);
+            tmpidx{c,1} = tp(t):tp(t)+n_trials-1;
+            pseudo = tmpdata(tmpidx{c,:});
+            pseudo = squeeze(nanmean(pseudo,1));
+            avedata(t,:) = pseudo(:);
             
         end;
+        
+        condata = cat(1, condata, avedata);
+                
+        if p==1 %no need to do this for each permutation
+            newlabels = [newlabels; c*ones(size(avedata,1),1)]; %#ok<AGROW>
+        end;
+    
     end;
+    
+    newdata(p,:,idx{1,2:end}) = condata;
     
 end;
 
 %reshape into data*pseudotrials (over permutations)
-newsz = size(newdata); newsz(2) = newsz(2)*newsz(3); newsz(3) = [];
-newdata = reshape(newdata,newsz);
 newsz = 1:length(newsz); newsz(length(newsz)+1) = 2; newsz(2)=[]; 
 newdata = permute(newdata, newsz); %trials are last dim
-newdata = squeeze(newdata); %one condition case
-
+newdata = squeeze(newdata); %one permutation case
 
 end
