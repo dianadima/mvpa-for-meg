344e8ffd17199a65b08c8f684e3a52e40d63b6a2
c1465333 <dimadc@cardiff.ac.uk>
2018-02-12 18:56:52 +0000

    Add subfolder of core decoding functions and objects

---
+++ Decode/Decode_core/decoding_args.m
@@ -0,0 +1,28 @@
+classdef decoding_args
+    
+    properties (Access = public)
+        
+        %default settings for time-resolved decoding
+        channels = 'MEG';
+        decoding_window = [];
+        window_length = 1;
+        time = [];
+        
+    end
+    
+    methods
+        
+        function obj = decoding_args(varargin)
+            
+            if ~isempty(varargin)
+                obj.channels = varargin{1};
+                obj.decoding_window = varargin{2};
+                obj.window_length = varargin{3};
+                obj.time = varargin{4};
+            end;
+        end
+        
+    end
+        
+end
+    
