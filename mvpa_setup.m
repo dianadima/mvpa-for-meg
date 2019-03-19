ddf517b269c4df21a671de53a8313d63ef6c2062
c1465333 <DimaDC@cardiff.ac.uk>
2019-03-19 16:23:45 +0000

    add directory

--- mvpa_setup.m
+++ mvpa_setup.m
@@ -3,15 +3,16 @@ fprintf('\nThese scripts were tested with Matlab R2015a and Fieldtrip-20190205.\
 toolbox_path = strrep(mfilename('fullpath'),'mvpa_setup','');
 
 %add dependencies with subdirectories
-addpath(genpath(fullfile(toolbox_path,'liblinear-2.11')));
-addpath(genpath(fullfile(toolbox_path,'libsvm')));
+addpath(genpath(fullfile(toolbox_path,'liblinear-2.11')))
+addpath(genpath(fullfile(toolbox_path,'libsvm')))
 
 %ensure only first-level directories (and not subfolders) are added to path
-addpath(fullfile(toolbox_path, 'Prepare'));
-addpath(fullfile(toolbox_path, 'Decode'));
-addpath(fullfile(toolbox_path, 'Plot'));
-addpath(fullfile(toolbox_path, 'Stats')); 
-addpath(fullfile(toolbox_path, 'RSA'));
+addpath(fullfile(toolbox_path, 'Prepare'))
+addpath(fullfile(toolbox_path, 'Prepare', 'beamforming'))
+addpath(fullfile(toolbox_path, 'Decode'))
+addpath(fullfile(toolbox_path, 'Plot'))
+addpath(fullfile(toolbox_path, 'Stats')) 
+addpath(fullfile(toolbox_path, 'RSA'))
 
 %subfolders contain less recommended versions of scripts - run them by
 %navigating to the subfolders.
