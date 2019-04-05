b45160dc04fdf49de30f3fc43b202b7f06857c23
c1465333 <DimaDC@cardiff.ac.uk>
2019-04-05 15:46:22 +0100

    update paths

--- Prepare/private/source_to_aal.m
+++ Prepare/private/source_to_aal.m
@@ -1,29 +1,29 @@
 function [roi_idx, labels] = source_to_aal(sourcemodel)
 %Outputs indices of inside sources in fieldtrip beamformer grid that fall within each of 90 AAL ROIs.
-%Inputs: - sourcemodelfile is full path of sourcemodel or cortical sheet, e.g. fieldtrip/template/sourcemodel/standard_sourcemodel10mm.
+%Inputs: - sourcemodel: sourcemodel or cortical sheet or full path to it (e.g. 'fieldtrip/template/sourcemodel/standard_sourcemodel3d10mm').
 %
 %DC Dima 2017 (diana.c.dima@gmail.com)
 
 [~, ftdir] = ft_version; %get FT directory
-atlas = ft_read_atlas([ftdir '/template/atlas/aal/ROI_MNI_V4.nii']); %load AAL atlas
+atlas = ft_read_atlas(fullfile(ftdir, 'template', 'atlas', 'aal', 'ROI_MNI_V4.nii')); %load AAL atlas
+atlas = ft_convert_units(atlas, 'mm');
 
 if ischar(sourcemodel)
     [~,~,ext] = fileparts(sourcemodel);
     if strcmp(ext, '.mat')
-        load(sourcemodel); %load sourcemodel
+        load(sourcemodel); %load FT default sourcemodel
     else
         sourcemodel = ft_read_headshape(sourcemodel);
         sourcemodel.inside = true(size(sourcemodel.pos,1),1);
-    end;
-end;
+    end
+end
 sourcemodel = ft_convert_units(sourcemodel,'mm');
 
 cfg = [];
 cfg.interpmethod = 'nearest';
 cfg.parameter = 'tissue';
 sourcemodel2 = ft_sourceinterpolate(cfg, atlas, sourcemodel); %interpolate grid to atlas
-a = 1:length(sourcemodel.inside);
-inside = a(sourcemodel.inside);        %we want indices for the inside sources only (3,294 for a 10 mm grid)
+inside = find(sourcemodel.inside);        %we want indices for the inside sources only (3,294 for a 10 mm grid)
 
 roi_idx = cell(1,90);
 
@@ -33,14 +33,14 @@ for i = 1:90                                %loop through ROIs
     
     for j = 1:length(indx)
         roi_idx{i}(j) = find(inside==indx(j)); %store indices
-    end;
-end;
+    end
+end
 
 labels = sourcemodel2.tissuelabel(1:90);
 
 if isfield(sourcemodel, 'tri') %if it's a cortical surface, remove deep structures
     roi_idx(71:80) = [];
     labels(71:80) = [];
-end;
+end
 
 end
